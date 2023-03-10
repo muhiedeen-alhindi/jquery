b3583499a4af8e498f5652e10735429e44e418c8
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-03-10 16:44:21 +0200

    USE jquery

--- frontend/js.js
+++ frontend/js.js
@@ -30,32 +30,29 @@ let html=''
 // init();
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    const container = document.getElementById('container');
+$(document).ready(function() {
+    
     axios({
-        method:"get",
-        url : "http://localhost/jquery/backend/php.php/ "
-      }).then(function (res) {
-        console.log(res.data);
-        for(i = 0; i < res.data.length; i++){
-            let id = res.data[i].id;
-            // let image = res.data[i].image;
-            let title = res.data[i].title;
-            let content = res.data[i].content;
-            let auther =res.data[i].auther;
-            let published_date=res.data[i].published_date;
-              html=`<div class= "container">`
-              html +=`<div> <p> ${id}</p> </div> `
-              html +=`<div> <p> ${title}</p> </div> `
-              html +=`<div> <p> ${content}</p> </div> `
-              html +=`<div> <p> ${auther}</p> </div> `
-              html +=`<div> <p> ${published_date}</p> </div> `
-              html+=`</div>`
-              container.insertAdjacentHTML('beforeend', html);
-             console.log(html)
-            
-        }
-       
-        
-      })
+      method:"get",
+      url : "http://localhost/jquery/backend/php.php/"
+    }).then(function (res) {
+      console.log(res.data);
+      for(i = 0; i < res.data.length; i++){
+        let id = res.data[i].id;
+        let title = res.data[i].title;
+        let content = res.data[i].content;
+        let auther =res.data[i].auther;
+        let published_date=res.data[i].published_date;
+        html=`<div class= "container">`
+        html +=`<div> <p> ${id}</p> </div> `
+        html +=`<div> <p> ${title}</p> </div> `
+        html +=`<div> <p> ${content}</p> </div> `
+        html +=`<div> <p> ${auther}</p> </div> `
+        html +=`<div> <p> ${published_date}</p> </div> `
+        html+=`</div>`
+        $("#container").append(html);  
+        console.log(html);
+      }
+    });
   });
+  
